f647917c4dcbbe34e41765c8d49e1368996be165
deni-stwn <denistwn67@gmail.com>
2026-01-07 13:08:36 +0700

    firebase setup

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYQfhtrxJmjksotey2stheX3_NtSusvXE",
-  authDomain: "react-expo-919c5.firebaseapp.com",
-  projectId: "react-expo-919c5",
-  storageBucket: "react-expo-919c5.firebasestorage.app",
-  messagingSenderId: "128204597058",
-  appId: "1:128204597058:web:25301d2e79cf092fa1cadb"
+  apiKey: "****************************************",
+  authDomain: "****************************************",
+  projectId: "****************************************",
+  storageBucket: "****************************************",
+  messagingSenderId: "****************************************",
+  appId: "****************************************"
 };
 
 const app = initializeApp(firebaseConfig);
